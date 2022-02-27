b1956803b2eef2fa42648000015f99929e841ca3
unknown <sabolc120@gmail.com>
2022-02-27 07:30:11 +0100

    myAutomata

---
+++ Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace automata
+{
+    class Calculator
+    {
+        public void Calculate(int cash, int order_price, int szazasok, int otvenesek, int huszasok, int tizesek, int otosok, int quantity)
+        {
+
+            cash -= order_price;
+            if (cash / 100 >= 1)
+            {
+
+                szazasok = cash / 100; //Elosztja százzal a bedobott pénzt
+                cash -= szazasok * 100; //Bedobott pénz: 1250, - 12 *100, akkor marad még 50 forint és tovább megy//az if branch
+            }
+            if (cash / 50 >= 1)
+            {
+                otvenesek = cash / 50;
+                cash -= otvenesek * 50;
+            }
+            if (cash / 20 >= 1)
+            {
+                huszasok = cash / 20;
+                cash -= huszasok * 20;
+            }
+            if (cash / 10 >= 1)
+            {
+                tizesek = cash / 10;
+                cash -= tizesek * 10;
+            }
+            if (cash / 5 >= 1)
+            {
+                otosok = cash / 5;
+                cash -= otosok * 5;
+            }
+            if(szazasok >=1 || otvenesek >=1 || huszasok >=1 || tizesek >=1 || otosok >= 1 || quantity <=20) //Ne fusson le feleslegesen ha nincsen visszajáró
+            {
+                Console.WriteLine("\n");
+                Console.WriteLine("Visszajáró százasokban: {0}", szazasok);
+                Console.WriteLine("Visszajáró ötvenesekben: {0}", otvenesek);
+                Console.WriteLine("Visszajáró huszasokban: {0}", huszasok);
+                Console.WriteLine("Visszajáró tizesekben: {0}", tizesek);
+                Console.WriteLine("Visszajáró ötösökben: {0}", otosok);
+            }
+        }
+        public void ellenorzo(int order_price, int cash)
+        {
+            if (cash < order_price)
+            {
+                Console.WriteLine("Nem elegendő pénz!");
+                Console.WriteLine("Bedobott pénz: {0}, rendelés összege: {1}", cash, order_price);
+                Console.WriteLine("Kilépés..Pénz kidobása.");
+            }
+        }
+        public void quantity_check(int quantity, string name)
+        {
+            if(quantity > 20)
+            {
+                Console.WriteLine("Nincen {0} elérhető {1} az automatában!(Out Of Stock)! Próbáljon meg kissebb mennyiségben rendelni.",quantity,name);
+                Console.WriteLine("Kilépés..");
+                System.Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace automata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator szamolo = new Calculator();
+            Console.WriteLine("\t 1. Kávé");
+            Console.WriteLine("\t 2. Tea");
+            Console.WriteLine("\t 3. Csoki szelet");
+            Console.WriteLine("\t 4. Tuc keksz");
+            Console.WriteLine("\t 5. Energiaital");
+            Console.WriteLine("\t 6. Víz");
+
+
+            int anwser = int.Parse(Console.ReadLine());
+            int quantity = 0, price_coffee = 200, price_tea = 150, price_chocobar = 300, price_biscuit = 350,
+                price_energydrink = 220, price_water = 150, order_price, cash;
+            int szazasok = 0, otvenesek = 0, huszasok = 0, tizesek = 0, otosok = 0;
+            String item = "";
+            bool machineOn = true;
+
+
+            while (machineOn)
+            {
+                if (anwser == 1)
+                {
+                    item = "Kávé";
+                    Console.Write("Mennyi {0}?: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_coffee * quantity; //Kiszámolja a teljes árat mennyiség és ár alapján       
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash); //Lefutattja azt az ellnőrzőtt ami megnézi elegendő a pénz.
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity); //Kiszámolja a visszajárót
+                }
+                if (anwser == 2)
+                {
+                    item = "Tea";
+                    Console.Write("Mennyi {0}: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_tea * quantity;
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash);
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity);
+                }
+                if (anwser == 3)
+                {
+                    item = "Csoki szelet";
+                    Console.Write("Mennyi {0}?: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_chocobar * quantity; //Kiszámolja a teljes árat mennyiség és ár alapján
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash);
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity);
+                }
+                if (anwser == 4)
+                {
+                    item = "Tuc keksz";
+                    Console.Write("Mennyi {0}?: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_biscuit * quantity; //Kiszámolja a teljes árat mennyiség és ár alapján
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash);
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity);
+                }
+                if (anwser == 5)
+                {
+                    item = "Energiaital";
+                    Console.Write("Mennyi {0}?: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_energydrink * quantity; //Kiszámolja a teljes árat mennyiség és ár alapján
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash);
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity);
+                }
+                if (anwser == 6)
+                {
+                    item = "Vizes palack";
+                    Console.Write("Mennyi {0}?: ",item);
+                    quantity = int.Parse(Console.ReadLine());
+                    szamolo.quantity_check(quantity, item);
+
+                    order_price = price_water * quantity; //Kiszámolja a teljes árat mennyiség és ár alapján
+                    Console.WriteLine("Rendelés összege ennyi lesz: {0}", order_price);
+
+                    Console.Write("Kérem, dobja be a pénzt: ");
+                    cash = int.Parse(Console.ReadLine());
+
+                    szamolo.ellenorzo(order_price, cash);
+                    szamolo.Calculate(cash, order_price, szazasok, otvenesek, huszasok, tizesek, otosok, quantity);
+                }
+                machineOn = false;
+            }
+        }
+    }
+}
